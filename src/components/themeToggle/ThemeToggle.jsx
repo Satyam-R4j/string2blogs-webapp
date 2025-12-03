0c0f163044a9219a22924bf5e139fa02b289a6c6
Satyam-R4j <satyamraj418@gmail.com>
2025-12-03 21:27:08 +0530

    Working on NavBar and theme Toggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import styles from "./themeToggle.module.css";
-
+import Image from "next/image";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+    const { theme } = useContext(ThemeContext);
+
+    console.log(theme);
+
+    return (
+        <div className={styles.container}>
+            <Image src="/moon.png" alt="" width={14} height={14} />
+            <div className={styles.ball}></div>
+            <Image src="/sun.png" alt="" width={14} height={14} />
+        </div>
+    );
 };
 
 export default ThemeToggle;
