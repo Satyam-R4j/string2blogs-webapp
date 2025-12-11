d14079cc53c285d7bbfcce528e3649539b8a92a3
Satyam-R4j <satyamraj418@gmail.com>
2025-12-11 09:51:44 +0530

    Working on Pagination & Footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className={styles.container}>
+    <div className={styles.info}>
+      <div className={styles.logo}>
+        <Image src="/logo.png" alt="lill Blog" width={50} height={50}/>
+        <h1 className={styles.logoText}>LiLLBlogs</h1>
+      </div>
+      <p className={styles.desc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus rerum ipsa doloremque quas provident possimus voluptates nam voluptate quam eos, laborum molestias? Harum accusantium delectus sed repellendus quod voluptatibus tenetur rerum sequi?
+      </p>
+      <div className={styles.icons}>
+        <Image src="/instagram.png" alt="" width={18} height={18}/>
+        <Image src="/linkedin.png" alt="" width={18} height={18}/>
+        <Image src="/twitter.png" alt="" width={18} height={18}/>
+
+      </div>
+    </div>
+    <div className={styles.links}>
+      <div className={styles.list}>
+        <span className={styles.listTitle}>Links</span>
+        <Link href="/">Home</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      <div className={styles.list}>
+        <span className={styles.listTitle}>Tags</span>
+        <Link href="/">Coding</Link>
+        <Link href="/">Fashion</Link>
+        <Link href="/">Coding</Link>
+        <Link href="/">Travel</Link>
+      </div>
+      <div className={styles.list}>
+        <span className={styles.listTitle}>Social</span>
+        <Link href="/">Instagram</Link>
+        <Link href="/">{"Twitter(X)"}</Link>
+        <Link href="/">Linkedin</Link>
+      </div>
+    </div>
+
+
+
+  </div>;
 };
 
 export default Footer;
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -3,7 +3,10 @@ import styles from "./pagination.module.css";
 
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  return <div className={styles.container}>
+    <button className={styles.button}>Previous</button>
+    <button className={styles.button}>Next</button>
+  </div>;
 };
 
 export default Pagination;
