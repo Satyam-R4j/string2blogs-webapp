15e5fc8ac6dacb391b53417e33c74efe7611d17c
Satyam-R4j <satyamraj418@gmail.com>
2025-12-15 17:58:35 +0530

    editor page complete

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,18 +1,39 @@
-"use client"
+"use client";
+
 import styles from "./writePage.module.css";
 import Image from "next/image";
-import {useState} from "react";
+import { useState } from "react";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
 
 const WritePage = () => {
-  const [open, setOpen] = useState(false);
-  
+    const [open, setOpen] = useState(false);
+
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Placeholder.configure({
+                placeholder: "Tell your story...",
+            }),
+        ],
+        content: "",
+        immediatelyRender: false,
+    });
+
     return (
         <div className={styles.container}>
-            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Title" className={styles.input} />
+
             <div className={styles.editor}>
-                <button className={styles.button}>
-                    <Image src="/plus.png" alt="" width={16} height={16} />
+                <button
+                    className={styles.button}
+                    onClick={() => setOpen(!open)}
+                >
+                    <Image src="/plus2.png" alt="" width={16} height={16} />
                 </button>
+
                 {open && (
                     <div className={styles.add}>
                         <button className={styles.addButton}>
@@ -41,7 +62,11 @@ const WritePage = () => {
                         </button>
                     </div>
                 )}
+
+                {/* TipTap Editor */}
+                <EditorContent className={styles.textArea} editor={editor} />
             </div>
+            <button className={styles.publish}>Publish</button>
         </div>
     );
 };
